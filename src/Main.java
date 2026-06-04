0fc82dd2c244d550ac8eaab92c068e59d89e2fa5
luccaves <lucasworkmail26@gmail.com>
2026-06-03 21:18:13 -0300

    Implementação da opção de sair do sistema.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import com.sun.source.tree.WhileLoopTree;
-
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
     //Inicialização dos OBJETOS
     static Scanner userInput = new Scanner(System.in);
-    static ArrayList<String> shoppinngList = new ArrayList<String>();
+    static ArrayList<String> shoppinngList = new ArrayList<>();
 
     //Inicialização de VARIÁVEIS
     static boolean systemRun = true;
@@ -19,11 +16,11 @@ public class Main {
         System.out.println("        LISTA DE COMPRAS");
         System.out.println("--------------------------------");
         System.out.println("Escolha a opção desejada:");
-        System.out.println("0 -> SAIR");
+        System.out.println("0 -> SAIR (Encerra o programa)");
         System.out.println("1 -> EXIBIR LISTA");
-        System.out.println("2 -> CRIA LISTA");
-        System.out.println("3 -> EDITAR LISTA");
-        System.out.println("4 -> EXCLUIR LISTA");
+        System.out.println("2 -> CRIAR LISTA");
+//        System.out.println("3 -> EDITAR LISTA");
+//        System.out.println("4 -> EXCLUIR LISTA");
 
     }
     public static void main(String[] args) {
@@ -32,6 +29,13 @@ public class Main {
             showPrincipalMenu();
             System.out.print(">>> ");
             optionMenu = userInput.nextInt();
+
+            switch (optionMenu) {
+                case 0:
+                    System.out.println("Programa encerrado...");
+                    systemRun = false;
+                    break;
+            }
         }
 
 
