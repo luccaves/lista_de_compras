c47195cccc14b3bd6bb1252155252c73c09db37b
luccaves <lucasworkmail26@gmail.com>
2026-06-05 01:35:20 -0300

    Implementação do sistema de adicionar itens a lista

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     //Inicialização dos OBJETOS
     static Scanner userInput = new Scanner(System.in);
-    static ArrayList<String> shoppinngList = new ArrayList<>();
+    static ArrayList<String> shoppingList = new ArrayList<>();
 
     //Inicialização de VARIÁVEIS
     static boolean systemRun = true;
@@ -12,6 +12,7 @@ public class Main {
     static String item;
 
 
+    ///Métodos principais
     //Métodos de visualização de MENU
     static void showPrincipalMenu() {
         System.out.println("--------------------------------");
@@ -20,40 +21,54 @@ public class Main {
         System.out.println("Escolha a opção desejada:");
         System.out.println("0 -> SAIR (Encerra o programa)");
         System.out.println("1 -> EXIBIR LISTA");
-        System.out.println("2 -> CRIAR LISTA");
+        System.out.println("2 -> ADICIONAR ITEM A LISTA");
 //        System.out.println("3 -> EDITAR LISTA");
 //        System.out.println("4 -> EXCLUIR LISTA");
 
     }
 
-    ///Métodos principais
     //Exibir os itens da lista
     static void showList() {
 
         //checa se a lista ja existe antes de exibi-la na tela
-        if (shoppinngList.isEmpty()) {
+        if (shoppingList.isEmpty()) {
             System.out.println("A lista esta vázia.");
 
         } else {
-            for (int indiceItem = 0; indiceItem < shoppinngList.size(); indiceItem++) {
+            for (int indiceItem = 0; indiceItem < shoppingList.size(); indiceItem++) {
                 System.out.printf("%d - ", indiceItem+1);
-                System.out.printf("%s\n",shoppinngList.get(indiceItem));
+                System.out.printf("%s\n", shoppingList.get(indiceItem));
             }
         }
 
     }
 
     // Adicionar itens  alista
-    static void addNewItem(String item) {
-        shoppinngList.add(item);
+    static void addNewItem() {
+        boolean goBack = false;
+        while (!goBack) {
+            System.out.println("Digite o nome do produto ('<b' para voltar): ");
+            item = userInput.nextLine();
+
+            if (item.equals("<b")) {
+                goBack = true;
+            } else if (item.isBlank()) {
+                System.out.println("Digite um valor váligo.");
+            } else {
+                shoppingList.add(item);
+            }
+        }
     }
 
+    //Sistema principal
     public static void main(String[] args) {
 
         while (systemRun) {
             showPrincipalMenu();
             System.out.print(">>> ");
             optionMenu = userInput.nextInt();
+            // Limpa o resíduo de buffer do Scanner
+            userInput.nextLine();
 
             // Verifica a escolha no MENU principal
             switch (optionMenu) {
@@ -61,13 +76,15 @@ public class Main {
                 case 0:
                     System.out.println("Programa encerrado...");
                     systemRun = false;
-
+                    break;
                 case 1:
                     showList();
-
+                    break;
+                case 2:
+                    addNewItem();
+                    break;
             }
         }
-
 
     }
 
